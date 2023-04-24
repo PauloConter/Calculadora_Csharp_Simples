5f19aa595056375cfc5d24e260c303495072f1ed
Paulo Conter <conterdev@gmail.com>
2023-04-23 21:23:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+
+
+        }
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o NÚMERO da opção desejada:");
+            Console.WriteLine("1- Soma");
+            Console.WriteLine("2- Subtração");
+            Console.WriteLine("3- Dvisão");
+            Console.WriteLine("4- Multiplicação");
+            Console.WriteLine("5- Sair");
+
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Selecione uma das opções á cima: ");
+            short resposta = short.Parse(Console.ReadLine());
+
+            switch (resposta)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+
+
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor: ");
+
+            // ConsoleReadLine só retorna strings foi feito conversao para Float.Parse para voltar um numero flutuante.
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+
+            // Console.WriteLine("O resultado da soma é: " + resultado);
+
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+
+            // Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+
+            // Console.WriteLine("O resultado da soma é: " + (v1 + v2));
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro número:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo número:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+
+            Console.WriteLine($"O resultado de subtração dos múmeros é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
+
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro número:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+
+            Console.WriteLine("O resultado da divisão é " + resultado);
+
+            Console.ReadKey();
+            Menu();
+
+
+
+
+
+
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro número:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+
+            Console.WriteLine($"O resultado da multiplicação entre os dois números é: {resultado}");
+
+            Console.ReadKey();
+            Menu();
+
+
+
+        }
+
+
+    }
+
+}
